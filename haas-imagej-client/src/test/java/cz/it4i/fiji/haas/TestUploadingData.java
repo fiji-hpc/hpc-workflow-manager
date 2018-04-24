4012f516afb95be0555976081ea19661673ff1c3
Jan Kožusznik <jan@kozusznik.cz>
2018-04-24 09:25:40 +0200

    fix: test

--- haas-imagej-client/src/test/java/cz/it4i/fiji/haas/TestUploadingData.java
+++ haas-imagej-client/src/test/java/cz/it4i/fiji/haas/TestUploadingData.java
@@ -1,4 +1,6 @@
 package cz.it4i.fiji.haas;
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,7 +9,7 @@ import cz.it4i.fiji.haas_java_client.HaaSClient.UploadingFile;
 
 public class TestUploadingData {
 	private static Logger log = LoggerFactory.getLogger(TestUploadingData.class);
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		UploadingFile uf = new UploadingFileFromResource("", "config.yaml");
 		log.info("size: " + uf.getLength());
 	}
