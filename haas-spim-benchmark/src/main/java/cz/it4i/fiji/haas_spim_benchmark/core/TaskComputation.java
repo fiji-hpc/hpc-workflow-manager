79db73b16972e1bd52e408c3f6366778d5f4c3d7
Jan Kožusznik <jan@kozusznik.cz>
2018-06-08 01:19:01 +0200

    fix: done task should not be in queue state

--- haas-spim-benchmark/src/main/java/cz/it4i/fiji/haas_spim_benchmark/core/TaskComputation.java
+++ haas-spim-benchmark/src/main/java/cz/it4i/fiji/haas_spim_benchmark/core/TaskComputation.java
@@ -185,7 +185,8 @@ public class TaskComputation {
 
 		// Should the state be queued, try to find out whether a log file exists
 		if (state == JobState.Queued) {
-			if (null != logs && !logs.stream().anyMatch(logFile -> computationAccessor.fileExists(logFile))) {
+			if (!taskDescription.equals(Constants.DONE_TASK) && null != logs
+					&& !logs.stream().anyMatch(logFile -> computationAccessor.fileExists(logFile))) {
 				return; // No log file exists yet
 			}
 			state = JobState.Running;
