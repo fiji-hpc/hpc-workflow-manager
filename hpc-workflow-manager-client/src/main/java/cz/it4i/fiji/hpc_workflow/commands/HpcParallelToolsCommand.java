9b8adfbb783bd3c1edc71ec57cc3d6eba2af5b5d
Dimitrios Stefanos Velissariou <velissarious@gmail.com>
2021-11-09 15:35:42 +0100

    show command in the update site's menu

---
+++ hpc-workflow-manager-client/src/main/java/cz/it4i/fiji/hpc_workflow/commands/HpcParallelToolsCommand.java
@@ -0,0 +1,32 @@
+
+package cz.it4i.fiji.hpc_workflow.commands;
+
+import org.scijava.Context;
+import org.scijava.Priority;
+import org.scijava.command.Command;
+import org.scijava.parallel.ParallelService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import cz.it4i.fiji.hpc_workflow.core.Constants;
+
+// This calls the ManageHPCWorkflow class.
+
+@Plugin(type = Command.class, headless = false, priority = Priority.HIGH,
+	menuPath = "Plugins>" + Constants.MENU_ITEM_NAME + ">" +
+		Constants.PLUGIN_NAME)
+public class HpcParallelToolsCommand implements Command {
+
+	@Parameter
+	private Context context;
+
+	@Parameter
+	private ParallelService parallelService;
+
+	@Override
+	public void run() {
+		ManageHPCWorkflow mhw = new ManageHPCWorkflow();
+		mhw.runWithGivenParameterValues(this.context, this.parallelService);
+	}
+
+}
