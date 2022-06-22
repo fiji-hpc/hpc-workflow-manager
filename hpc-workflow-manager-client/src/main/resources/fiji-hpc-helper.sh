205d54fab9e3c0664bef51c5f4d4a24a8fbd2b2f
Dimitrios Stefanos Velissariou <velissarious@gmail.com>
2022-06-22 19:17:51 +0200

    add test for read right for scheduler directory in deployment script

--- hpc-workflow-manager-client/src/main/resources/fiji-hpc-helper.sh
+++ hpc-workflow-manager-client/src/main/resources/fiji-hpc-helper.sh
@@ -217,11 +217,13 @@ tar xvfz openmpi-${OPENMPI_VERSION}.tar.gz
 ##rm -r openmpi-${OPENMPI_VERSION}.tar.gz
 
 # Scheduler directory must exist.
-if [ -d "$DIR" ]; then
+if [ -d "$DIR" ] && [ -r "$DIR" ]
+then
   write_item "Scheduler directory $DIR found!"
   configure_and_install_open_mpi
 else
-  write_error "Scheduler directory $DIR must exist! Try running this script in an interactive job."
+  write_error "Scheduler directory $DIR must exist and be accessible!"
+  write_item "Try running this script in an interactive job. In PBS for example run: qsub -q qexp -l select=1 -I"
   exit 1
 fi
 
